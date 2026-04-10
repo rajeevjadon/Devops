3674235d60abbaf716f30104401195d38f25feb2
rajeevjadon <rajeevjadon28@gmail.com>
2026-04-11 00:46:09 +0530

    updated

--- CICD/myinfraPipeline/env/dev/main.tf
+++ CICD/myinfraPipeline/env/dev/main.tf
@@ -4,6 +4,14 @@ module "resource_group" {
   location = var.location
 }
 
+
+#This is the Simran RG
+module "resource_group" {
+  source   = "../../modules/resource_group"
+  rg_name  = "simranRG"
+  location = var.location
+}
+
 module "network" {
   source     = "../../modules/network"
   depends_on = [module.resource_group]
